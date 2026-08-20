b9bacbfdf112572ed495f67d76a3ca40634e6659
Nelakurthi Subhash <nelakurthisubhash@gmail.com>
2026-08-20 12:48:16 +0530

    Another basic Java code

---
+++ j1001.java
@@ -0,0 +1,15 @@
+class A {
+   public static void main(String[] args) {
+       int a=10;
+       int b=5;
+       if(a>b){
+            System.out.println("A is big");
+       }
+       else if(a==b){
+            System.out.println("Both are Equal");
+       }
+       else{
+            System.out.println("B is big");
+       }
+   }
+}
