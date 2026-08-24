34f55f2f4182a42f1611d9ae0f70fbc1bdfe729b
Nelakurthi Subhash <nelakurthisubhash@gmail.com>
2026-08-24 22:25:45 +0530

    Added Input Validation

--- BranchFlow.java
+++ BranchFlow.java
@@ -3,6 +3,10 @@ class A{
 	public static void main(String[]args){
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
+		if(n<=0){
+			System.out.println("Enter Number greater than 1");
+			return;
+		}
 		int count=0;
 		for(int i=1;i<=n;i++){
 			if(n%i==0){
