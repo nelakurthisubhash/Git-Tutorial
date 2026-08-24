1be34fbbce15e880bc906f5216d989a58e26778f
Nelakurthi Subhash <nelakurthisubhash@gmail.com>
2026-08-24 22:50:43 +0530

    Added OptimalCode

--- BranchFlow.java
+++ BranchFlow.java
@@ -8,13 +8,13 @@ class A{
 			return;
 		}
 		int count=0;
-		for(int i=1;i<=n;i++){
+		for(int i=2;i<=n/2;i++){
 			if(n%i==0){
 				count++;
 			}
 			else{}
 		}
-		if(count==2){
+		if(count==0){
 			System.out.println("Prime Number");
 		}
 		else{
