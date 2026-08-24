585b9bb570c02940849e53420910d3f422350737
Nelakurthi Subhash <nelakurthisubhash@gmail.com>
2026-08-24 22:17:52 +0530

    Example for Branch WorkFlow

---
+++ BranchFlow.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class A{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int count=0;
+		for(int i=1;i<=n;i++){
+			if(n%i==0){
+				count++;
+			}
+			else{}
+		}
+		if(count==2){
+			System.out.println("Prime Number");
+		}
+		else{
+			System.out.println("Not a prime Number");
+		}
+	}
+}
