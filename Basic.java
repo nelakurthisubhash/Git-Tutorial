2789cd1cb2f452dee1e1fae6ca79faf2bdcdb985
Nelakurthi Subhash <nelakurthisubhash@gmail.com>
2026-08-25 22:53:56 +0530

    for git 1st branch feature

--- Basic.java
+++ Basic.java
@@ -2,5 +2,7 @@
 class A {
     public static void main(String[] args) {
         System.out.println("This is basic java Code for github purpose");
+	System.out.println("This is extraprintln statements");
+	
     }
 }
